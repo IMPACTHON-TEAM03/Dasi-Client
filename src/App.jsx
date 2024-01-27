48fc993404b8990d8f581fd4c2733a3dbc11475c
MinJun Park <gusoong0829@naver.com>
2024-01-28 08:54:18 +0900

    TEST

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Service from "./pages/Service";
 import Region from "./pages/Region";
-import Locate from "./pages/Locate";
 import Detail from "./pages/Detail";
 import Property from "./pages/Property";
 
@@ -15,7 +14,6 @@ const App = () => {
           <Route path="/" element={<Main />} />
           <Route path="/service" element={<Service />} />
           <Route path="/region" element={<Region />} />
-          <Route path="/locate" element={<Locate />} />
           <Route path="/detail" element={<Detail />} />
           <Route path="/property" element={<Property />} />
         </Routes>
